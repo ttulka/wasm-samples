19b3cef604aa97f3662628d19a44f4fb7e054e39
Tomas Tulka <tomas.tulka@gmail.com>
2021-01-20 20:58:20 +0100

    assemblyscript string

--- assemblyscript/assembly/index.ts
+++ assemblyscript/assembly/index.ts
@@ -1,4 +1,5 @@
 declare function log(n: i32): void;
+declare function logstr(s: string): void;
 
 export function add(a: i32, b: i32): i32 {
   return a + b;
@@ -6,4 +7,8 @@ export function add(a: i32, b: i32): i32 {
 
 export function main(): void {
 	log(42);
+}
+
+export function hello(): void {
+  logstr("Hello, world!");
 }
--- assemblyscript/index.js
+++ assemblyscript/index.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 const loader = require("@assemblyscript/loader");
-const imports = { 
+const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), { 
   "index": {
     log: function(value) {
         console.log(value);
+    },
+    logstr: function(value) {
+        console.log(wasm.exports.__getString(value));
     }
   }
-};
-const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-//module.exports = wasm.exports;
+});
+
+console.log(loader);
 
 console.log(wasm.instance.exports.add(2,3));
 wasm.exports.main();
+wasm.exports.hello();
